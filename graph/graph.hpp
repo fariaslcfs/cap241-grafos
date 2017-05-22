8f4e14f125039c3cc2f7d1e4f88bd98ddeba46d4
Eduardo Pereira de Sousa <edupsousa@gmail.com>
2017-05-22 19:53:54 -0300

    Movidas funções legadas na linguagem C para arquivo próprio

--- graph/graph.hpp
+++ graph/graph.hpp
@@ -112,75 +112,10 @@ void Graph::print() {
     }
 }
 
+
 /*
 Funções legadas da implementação em C
 */
-
-Graph* create_graph(size_t size);
-void free_graph(Graph* g);
-Node *create_node(int id, float weight, Node *next);
-void free_node(Node* n);
-int insert_edge(Graph* g, int n1, int n2, float weight);
-int remove_edge(Graph* g, int n1, int n2);
-int exists_edge(Graph* g, int n1, int n2);
-Node *adjacent_edges(Graph* g, int n);
-void print_graph(Graph* g);
-Graph* create_demo_graph();
-
-Graph *create_graph(size_t size){
-    Graph* g = new Graph(size);
-    return g;
-}
-
-void free_graph(Graph* g){
-   delete g;
-}
-
-Node *create_node(int id, float weight, Node* next){
-    Node *n = new Node(id, weight, next);
-    return n;
-}
-
-void free_node(Node* n){
-    if(n != NULL){
-        delete n;
-    }
-}
-
-int insert_edge(Graph* g, int n1, int n2, float weight){
-    return g->insertEdge(n1, n2, weight);
-}
-
-int remove_edge(Graph* g, int n1, int n2) {
-    return g->removeEdge(n1, n2);
-}
-
-int exists_edge(Graph* g, int n1, int n2){
-    return g->existsEdge(n1, n2);
-}
-
-Node* adjacent_edges(Graph* g, int n){
-    return g->adjacentEdges(n);
-}
-
-void print_graph(Graph* g){
-    g->print();
-}
-
-Graph* create_demo_graph(){
-    Graph *g = create_graph(9);
-    
-    insert_edge(g, 0, 1, 1.);
-    insert_edge(g, 1, 2, 1.);
-    insert_edge(g, 2, 3, 1.);
-    insert_edge(g, 1, 4, 1.);
-    insert_edge(g, 4, 5, 1.);
-    insert_edge(g, 0, 5, 1.);
-    insert_edge(g, 0, 6, 1.);
-    insert_edge(g, 6, 7, 1.);
-    insert_edge(g, 6, 8, 1.);
-    
-    return g;
-}
+#include "graph_legacy.h"
 
 #endif /* __GRAPH_H__ */
